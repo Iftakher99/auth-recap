6c21d82dd05dce243307b73d5994504217e55c7c
Iftakher99 <19tonmoy99@gmail.com>
2021-03-16 11:04:34 +0600

    finished module 42.5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,115 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import firebase from "firebase/app";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import firebaseConfig from "./fireBase.config";
+import { useState } from "react";
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+// Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 function App() {
+  const [user, setUser] = useState({});
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+  const fbProvider = new firebase.auth.FacebookAuthProvider();
+  const githubProvider = new firebase.auth.GithubAuthProvider();
+  const handleGithubSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(githubProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+        var token = credential.accessToken;
+
+        // The signed-in user info.
+        var user = result.user;
+        setUser(user);
+        // ...
+        console.log(user, token);
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
+  const handleFbSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(fbProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // The signed-in user info.
+        var user = result.user;
+
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var accessToken = credential.accessToken;
+        setUser(user);
+        console.log(user, accessToken);
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email, credential);
+
+        // ...
+      });
+  };
+  const handleGoogleSignIn = () => {
+    firebase
+      .auth()
+      .signInWithPopup(googleProvider)
+      .then((result) => {
+        /** @type {firebase.auth.OAuthCredential} */
+        var credential = result.credential;
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        setUser(user);
+        console.log(token, user);
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+        console.log(errorCode, errorMessage, email, credential);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <button onClick={handleGoogleSignIn}>Google</button> <br />
+      <button onClick={handleFbSignIn}>Facebook</button>
+      <button onClick={handleGithubSignIn}>Github</button>
+      <h3>Email:{user.email}</h3>
+      <h3>Name:{user.displayName}</h3>
+      <img src={user.photoURL} alt='' />
     </div>
   );
 }
---
+++ src/fireBase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBKEU1MMOEHATWEXhnlKxCfAPaDkHzsPNY",
+  authDomain: "auth-recap-5e031.firebaseapp.com",
+  projectId: "auth-recap-5e031",
+  storageBucket: "auth-recap-5e031.appspot.com",
+  messagingSenderId: "18785875556",
+  appId: "1:18785875556:web:950c5095cc60cd6ca4d171",
+};
+export default firebaseConfig;
